d50195c1e45c0a882ec3d87432c196f6eb0618c5
waltershaquil <81990995+waltershaquil@users.noreply.github.com>
2025-04-05 12:22:18 +0200

    dockerized + db

--- src/main/java/com/learning/carsSales/Controller/SaleController.java
+++ src/main/java/com/learning/carsSales/Controller/SaleController.java
@@ -6,7 +6,6 @@ import com.learning.carsSales.Entity.Car;
 import com.learning.carsSales.Entity.CarRepository;
 import com.learning.carsSales.Entity.Sale;
 import com.learning.carsSales.Entity.SaleRepository;
-import com.sun.jdi.LongValue;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
